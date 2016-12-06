b9314d17645d3acbc7dce2cbeabda9a8b035edfa
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2016-12-06 16:47:16 -0500

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,45 +8,129 @@
 // - at the end of the game, show "all done, here's your final score", # of correct answers, # of incorrect answers, # of unanswered
 // - at end of game, also have a start over button, which resets the game
 
-var question;
+$(document).ready(function() {
+
+
+var questions = [
+		"What's the name of the bear Mowgli meets from The Jungle Book?", 
+		"What is the name of the prince that Ariel from The Little Mermaid falls in love with?", 
+		"What does 'Hakuna Matata' mean?", 
+		"What were Aladdin's 3 wishes?", 
+		"What is the Olaf from Frozen's favorite season?", 
+		"The Enchanted Rose in Beast's castle will continue to bloom until when?", 
+		"What is the name of the chracter who owns all the toys in Toy Story?", 
+		"What continent does Tarzan take place?"
+		]
+
+var	answers = [
+		["Bagheera", "Baloo", "Akela", "King Louie" ], 
+		["Prince John", "Prince Charming", "Prince Eric", "Prince Philip"], 
+		["'No Worries'", "'Long Live the King'", "'Lets be Friends'", "'Eat, Sleep, and Drink'" ], 
+		["To marry Jasmine, to be rescued from drowning, and to free the genie", "To marry Jasmine, to put Jafar in jail, and to free the genie", "To become a prince, to be rescued from drowning, and to gain Jasmine’s father’s blessing to marry his daughter", "To become a prince, to be rescued from drowning, and to free the genie"], 
+		["Winter", "Spring", "Summer", "Fall"], 
+		["His 21st Birthday", "His 18th Birthday", "True Love's First Kiss", "His Wedding Day"], 
+		["Jeff", "Robert", "Andy", "George"], 
+		["Asia", "Afria", "South America", "Australia"]
+		]
+
+var	correctChoice = [
+		"Baloo",
+		"Prince Eric",
+		"No Worries",
+		"To become a prince, to be rescued from drowning, and to free the genie",
+		"Summer",
+		"His 21st Birthday",
+		"Andy",
+		"Africa"
+	]
+
+var images = [
+	"../TriviaGame/assets/images/jungle_book.gif",
+	"../TriviaGame/assets/images/little_mermaid.gif",
+	"../TriviaGame/assets/images/lion_king.gif",
+	"../TriviaGame/assets/images/aladdin.gif",
+	"../TriviaGame/assets/images/olaf_frozen.gif",
+	"../TriviaGame/assets/images/beauty_and_the_beast.gif",
+	"../TriviaGame/assets/images/toy_story.gif",
+	"../TriviaGame/assets/images/tarzan.gif"
+]
+
+
+
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 var seconds = 30;
 var showAnswer;
 
-function startTrivia() {
-	
-	$("#start").remove();
+
+function startScreen() {
+	$("#start").html("<h2 class='btn btn-default'> Start </h2>")
+}
+startScreen();
+
+
+
+
+$(document).on("click", "#start", function() {
+
+	startTrivia(0);
+
+
+});
+
+
+function startTrivia(num) {
 
 	$("#timer").html("Time Remaining: " + seconds + " seconds")
-
-	$("#question-holder").html("What's the name of the bear Mowgli meets from The Jungle Book?");
-
-	var choices = ["Bagheera", "Baloo", "Akela", "King Loiue"];
-
-	for (i=0 ; i<choices.length ; i++) {
+	$("#question-holder").html(questions[0]);
+	for (i=0 ; i<answers[0].length ; i++) {
 		var chooseChoice = $("#answer-choices");
-		chooseChoice.append("<button> <h3>" + choices[i] + "</h3> </button>");
+		chooseChoice.append("<button> <h3 class='choice" + i + "'>" + answers[0][i] + "</h3> </button>");
 	}
+}
 
-	function timesOuts() {
-		$("#timer").html("Time Remaining: " + seconds + " seconds");
-		$("#answer-choices").remove();
-		$("#question-holder").html("Out of Time!!");
-		$("#correct-answer").html("The corret answer is Baloo");
-		$("#correct-image").append("<img src ='https://media.giphy.com/media/yl8OwPJyIIYhi/giphy.gif' alt='Jungle Book' />");
 
-	}
 
-	setTimeout(timesOuts, 1000 * 2); //testing timer
+// 		var crystalvalue = $(this).data("crystalvalue");
+// 		yourGuess += crystalvalue;
+// 		$('#guessedScore').html('Your score: ' + yourGuess);
+
+
+
+// function startTrivia() {
+	
+// 	$("#start").remove();
+
+// 	$("#timer").html("Time Remaining: " + seconds + " seconds")
+
+// 	$("#question-holder").html("What's the name of the bear Mowgli meets from The Jungle Book?");
+
+// 	var choices = ["Bagheera", "Baloo", "Akela", "King Loiue"];
+
+// 	for (i=0 ; i<choices.length ; i++) {
+// 		var chooseChoice = $("#answer-choices");
+// 		chooseChoice.append("<button> <h3>" + choices[i] + "</h3> </button>");
+// 	}
+
+// 	function timesOuts() {
+// 		$("#timer").html("Time Remaining: " + seconds + " seconds");
+// 		$("#answer-choices").remove();
+// 		$("#question-holder").html("Out of Time!!");
+// 		$("#correct-answer").html("The corret answer is Baloo");
+// 		$("#correct-image").append("<img src ='https://media.giphy.com/media/yl8OwPJyIIYhi/giphy.gif' alt='Jungle Book' class='image-class'/>");
+
+// 	}
+
+// 	setTimeout(timesOuts, 1000 * 2); //testing timer
 
 	
 
-}
+// }
 
-$("#start").click(startTrivia);
-
-
+// $("#start").click(startTrivia);
 
 
+
+
+});
